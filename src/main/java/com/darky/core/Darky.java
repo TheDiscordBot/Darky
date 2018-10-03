fe43f69eebf8479b204cfb222fdfd6db0ef9fe73
Stu <justuskliem@gmail.com>
2018-10-03 16:14:35 +0200

    Added Error Logging

--- src/main/java/com/darky/core/Darky.java
+++ src/main/java/com/darky/core/Darky.java
@@ -41,10 +41,11 @@ public class Darky extends ListenerAdapter {
         }
 
         shardManager.addEventListener(new RegisterListener(database));
-        CommandSettings settings = new CommandSettings(config.getPrefix(), shardManager, true);
-                settings.put(new HelpCommand(database), "help", "helpme")
-                        .put(new KickCommand(), "kick")
-                        .put(new RegisterCommand(database), "register")
+
+        CommandSettings settings = new CommandSettings(config.getPrefix(), shardManager, true, config);
+        settings.put(new HelpCommand(database), "help", "helpme")
+                .put(new KickCommand(), "kick")
+                .put(new RegisterCommand(database), "register")
                 .activate();
     }
 
---
+++ src/main/java/com/github/johnnyjayjay/discord/commandapi/CommandListener.java
@@ -0,0 +1,67 @@
+package com.github.johnnyjayjay.discord.commandapi;
+
+import com.darky.core.Config;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+
+class CommandListener extends ListenerAdapter {
+
+    private Config config;
+    private CommandSettings settings;
+    private Map<Long, Long> cooldowns; // Long: User id, Long: last timestamp
+
+    public CommandListener(CommandSettings settings, Config config) {
+        this.config = config;
+        this.settings = settings;
+        this.cooldowns = new HashMap<>();
+    }
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        TextChannel channel = event.getChannel();
+        if (!settings.getBlacklistedChannels().contains(channel.getIdLong()) && (!event.getAuthor().isBot() || settings.botsMayExecute())) {
+            String raw = event.getMessage().getContentRaw();
+            String prefix = settings.getPrefix(event.getGuild().getIdLong());
+            if (raw.startsWith(prefix)) {
+                long timestamp = System.currentTimeMillis();
+                long userId = event.getAuthor().getIdLong();
+                if (cooldowns.containsKey(userId) && (timestamp - cooldowns.get(userId)) < settings.getCooldown()) {
+                    if (settings.isResetCooldown())
+                        cooldowns.put(userId, timestamp);
+                    return;
+                }
+                cooldowns.put(userId, timestamp);
+                CommandEvent.Command cmd = CommandEvent.parseCommand(raw, prefix, settings);
+                if (cmd.getExecutor() != null) {
+                    try {
+                        cmd.getExecutor().onCommand(new CommandEvent(event.getJDA(), event.getResponseNumber(), event.getMessage(), cmd, settings),
+                                event.getMember(), channel, cmd.getArgs());
+                    } catch (Throwable t) {
+                        event.getJDA().getTextChannelById(config.getErrorChannel()).sendMessage(new EmbedBuilder()
+                                .setTitle("Error")
+                                .setDescription("**__User:__** " + event.getMember().getAsMention() + " `(" + event.getMember().getUser().getId() + ")`\n" +
+                                        "**__Guild:__** " + event.getGuild().getName() + " `(" + event.getGuild().getId() + ")`\n" +
+                                        "**__Command:__** " + cmd.getLabel())
+                                .addField("Error", "```css\n" + t.toString() + "\n```", false)
+                                .addField("Stacktrace", String.join("\n", Arrays.toString(Arrays.copyOfRange(t.getStackTrace(), 0, 5))), false).build()).queue();
+                    }
+                } else {
+                    Message unknownCommand = settings.getUnknownCommandMessage();
+                    if (unknownCommand != null && event.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_WRITE, Permission.MESSAGE_EMBED_LINKS))
+                        channel.sendMessage(unknownCommand).queue();
+                }
+            }
+        }
+    }
+}
